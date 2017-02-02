a4e216901842a0ae485c47370daceff6ea1768f2
fernand0martins <fmbr.martins@gmail.com>
2017-02-02 19:09:36 +0000

    added multiple service config and bash command status launcher

--- snitch.py
+++ snitch.py
@@ -1,4 +1,11 @@
-import ConfigParser
+import ConfigParser, os
 
 config = ConfigParser.ConfigParser()
 config.read("config/config.cnf")
+
+url = config.get("snitch", "url")
+service_names_array = config.get("snitch", "service_names").split(',')
+
+for service in service_names_array:
+    bashCommand = "service " + service + " status | grep \"Active: active (running)\""
+    os.system(bashCommand)
