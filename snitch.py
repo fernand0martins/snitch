0cd96163c9c0ca4a9a0d501788af442ab11f82ae
fernand0martins <fmbr.martins@gmail.com>
2017-02-02 19:20:22 +0000

    added exception handler on missing config file

--- snitch.py
+++ snitch.py
@@ -1,7 +1,13 @@
-import ConfigParser, os
+import ConfigParser
+import os
+
+config_file = "config/config.cnf"
+
+if not os.path.isfile(config_file):
+    raise Exception("Config file missing")
 
 config = ConfigParser.ConfigParser()
-config.read("config/config.cnf")
+config.read(config_file)
 
 url = config.get("snitch", "url")
 service_names_array = config.get("snitch", "service_names").split(',')
