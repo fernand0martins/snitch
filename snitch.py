d4d492bf0c2fa6bf63dcf942ac4f064b2dc81263
fernand0martins <fmbr.martins@gmail.com>
2017-02-03 18:43:26 +0000

    moved datetime now() into function

--- snitch.py
+++ snitch.py
@@ -4,27 +4,37 @@ import subprocess
 import requests
 import time
 
-script_dir = os.path.dirname(__file__)
-config_file = os.path.join(script_dir, "config/config.cnf")
 
-if not os.path.isfile(config_file):
-    print(time.strftime("%I:%M:%S") + ": Snitch config failed to load.")
-    raise Exception("Config file missing")
+def date():
+    return time.strftime("%I:%M:%S")
 
-config = ConfigParser.ConfigParser()
-config.read(config_file)
 
-url = config.get("snitch", "url")
-service_names_array = config.get("snitch", "service_names").split(',')
+def main():
+    script_dir = os.path.dirname(__file__)
+    config_file = os.path.join(script_dir, "config/config.cnf")
 
-for service in service_names_array:
-    bashCommand = "/bin/systemctl status " + service + " | grep -c \"Active: active\""
+    if not os.path.isfile(config_file):
+        print(date() + ": Snitch config failed to load.")
+        raise Exception("Config file missing")
 
-    result = subprocess.check_output(bashCommand, shell=True)
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
 
-    if result == 0:
-         print(time.strftime("%I:%M:%S") + ": Snitch failed.")
-         raise Exception("Service" + service + " is not running")
+    url = config.get("snitch", "url")
+    service_names_array = config.get("snitch", "service_names").split(',')
 
-requests.get(url)
-print(time.strftime("%I:%M:%S") + ": Snitch done.")
+    for service in service_names_array:
+        bashCommand = "/bin/systemctl status " + service + " | grep -c \"Active: active\""
+
+        result = subprocess.check_output(bashCommand, shell=True)
+
+        if result == 0:
+            print (date() + ": Snitch failed.")
+            raise Exception("Service" + service + " is not running")
+
+    requests.get(url)
+    print(date() + ": Snitch done.")
+
+    return
+
+main()
